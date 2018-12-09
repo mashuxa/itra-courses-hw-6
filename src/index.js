2dac7f65bb8bf393803a80fc55554041ac4d5aea
Your Name <you@example.com>
2018-12-09 23:50:46 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.scss';
-import App from './app/App';
+import App from './App';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.querySelector('.grid'));
